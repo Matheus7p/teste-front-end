82dbfcacac35e9fe1e258bdb05dbd40d0387c06a
Matheus7p <matheus.costa.7p@gmail.com>
2025-10-17 20:50:50 -0300

    feat: add input

---
+++ src/app/components/ui/input/input.ui.tsx
@@ -0,0 +1,23 @@
+import classNames from "classnames";
+import React from "react";
+
+import styles from "./input.module.scss"; 
+
+interface IInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  variant?: "default";
+  className?: string
+}
+
+export const Input : React.FC<IInputProps> = ({ variant = "default", className, ...props }) => {
+  return (
+    <input
+      className={classNames(
+        styles.input, 
+        styles[`input--${variant}`], 
+        className, 
+      )}
+      {...props}
+    />
+  );
+};
+
