7e2a1b45c778771cb3c6b22968d65b9d9a68da46
Matheus7p <matheus.costa.7p@gmail.com>
2025-10-20 03:11:29 -0300

    feat: add heroSection with tests

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,10 +1,21 @@
 import { ReactElement } from "react";
 
+import { HeroSection } from "@/app/components/heroSection/hero-section.component";
+import { Button } from "@/app/components/ui/button/button.ui";
+
 
 export default function Home (): ReactElement {
   return (
-    <div >
-      <h1>Teste Econverse</h1>
-    </div>
+    <main >
+      <HeroSection>
+        <div>
+          <h3>Venha conhecer nossas <br/> promoções</h3>
+          <p><span>50% Off</span> nos produtos</p>
+          <Button variant="yellow">
+          Ver Produto
+          </Button>
+        </div>
+      </HeroSection>
+    </main>
   );
 }
