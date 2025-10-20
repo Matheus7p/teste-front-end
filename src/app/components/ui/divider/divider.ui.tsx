873360a80bb9764b9c656463fae6c0733ef3e173
Matheus7p <matheus.costa.7p@gmail.com>
2025-10-19 21:31:40 -0300

    feat: divider

---
+++ src/app/components/ui/divider/divider.ui.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from "react";
+
+import styles from "./divider.module.scss";
+
+export const Divider = (): ReactElement => {
+  return ( <div className={styles.divider}></div> );
+};
+ 
