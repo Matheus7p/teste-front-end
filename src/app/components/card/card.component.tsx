225a51a9fc30f6699464ae1ed5aa286ceff442c4
Matheus7p <matheus.costa.7p@gmail.com>
2025-10-21 00:35:45 -0300

    feat: add card with tests

---
+++ src/app/components/card/card.component.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import React, { PropsWithChildren } from "react";
+
+
+import { Button } from "@/app/components/ui/button/button.ui";
+import { IProduct } from "@/app/types/products.type";
+import { formatPriceToBRL } from "@/utils/price-format.utils";
+
+import styles from "./card.module.scss";
+
+interface ICardImage {
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
+}
+interface ICard {
+  children: React.ReactNode;
+  className: string;
+
+}
+
+export const Card = ({ children, className }: ICard): React.ReactElement => {
+  return (<div className={className}>{children}</div>);
+};
+
+export const CardImage = ({ src, alt, width, height }: ICardImage): React.ReactElement => {
+  return (<Image src={src} alt={alt} width={width} height={height}/>);
+};
+
+export const CardContent = ({ children }: PropsWithChildren): React.ReactElement => {
+  return (<div>{children}</div>);
+};
+
+export const CardButton = ({ children }: PropsWithChildren): React.ReactElement => {
+  return (<div>{children}</div>);
+};
+
+export const CardProduct = ({ productName, price, descriptionShort, photo}: IProduct): React.ReactElement => {
+
+  return (
+    <Card className={styles.productCard as string}>
+      <CardImage src={photo} alt={productName} width={278.1} height={228}/>
+      <CardContent>
+        <p className={styles.productName}>{productName}</p>
+        <p className={styles.productName}>{descriptionShort}</p>
+        <p className={styles.priceDiscounted}>
+          <span style={{ textDecoration: "line-through" }}>
+            R$ {formatPriceToBRL(price)}
+          </span>
+          <br />
+          R$ {formatPriceToBRL(price)}
+        </p>
+        <p className={styles.priceInstallments}>
+        ou 2x de {formatPriceToBRL(price/2)} sem juros
+        </p>
+        <p>Frete grátis</p>
+      </CardContent>
+      <CardButton>
+        <Button variant="blue">COMPRAR</Button>
+      </CardButton>
+    </Card>);
+};
