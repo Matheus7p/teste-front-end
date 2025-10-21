a298340b931bdc708f3527ec70130af0f2a51dfe
Matheus7p <matheus.costa.7p@gmail.com>
2025-10-21 10:58:32 -0300

    feat: add page styles and update tests for product sections

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,13 +1,23 @@
-import { ReactElement } from "react";
+import { JSX } from "react";
 
+import { getProduct } from "@/app/actions/product.actions";
+import { CardProduct } from "@/app/components/card/card.component";
 import { CategoriesDisplayToHome } from "@/app/components/categories-display/categories-display.component";
 import { HeroSection } from "@/app/components/heroSection/hero-section.component";
+import { TabSelectorRelatedProduct } from "@/app/components/tab-selector/tab-selector.component";
+import { Alert } from "@/app/components/ui/alert/alert.ui";
 import { Button } from "@/app/components/ui/button/button.ui";
+import { Carousel } from "@/app/components/ui/carousel/carousel.ui";
+import { Divider } from "@/app/components/ui/divider/divider.ui";
 
+import styles from "./page.module.scss";
 
-export default function Home (): ReactElement {
+export default async function Home (): Promise<JSX.Element> {
+  const { products, error } = await getProduct();
+
   return (
     <main >
+
       <HeroSection>
         <div>
           <h3>Venha conhecer nossas <br/> promoções</h3>
@@ -17,7 +27,32 @@ export default function Home (): ReactElement {
           </Button>
         </div>
       </HeroSection>
+
       <CategoriesDisplayToHome />
-    </main>
+
+      <section className={styles.relatedProduct}>
+        <div className={styles.titleContainer}>
+          <Divider variant="relatedProduct"/>
+          <h2>Produtos Relacionados</h2>
+          <Divider variant="relatedProduct"/>
+        </div>
+
+        <TabSelectorRelatedProduct />
+
+        <div className={styles.productsListContainer}>
+          {error ? (
+            <Alert variant="error" title="Ocorreu um Erro">
+              {error}
+            </Alert>) 
+            : (
+              <Carousel>
+                {products.map((product) => (
+                  <CardProduct key={product.productName} productName={product.productName} price={product.price} descriptionShort={product.descriptionShort} photo={product.photo} />
+                ))}
+              </Carousel>
+            )}
+        </div>
+      </section>
+    </main> 
   );
 }
