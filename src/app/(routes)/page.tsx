a52dc5d70ce7c5eb9bd49d4ea92b51385fbb9979
Matheus7p <matheus.costa.7p@gmail.com>
2025-10-21 21:00:35 -0300

    style: improve spacing beetwen components

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,14 +1,18 @@
+import Link from "next/link";
 import { JSX } from "react";
 
 import { getProduct } from "@/app/actions/product.actions";
+import { Brands } from "@/app/components/brands/brands.component";
 import { CardProduct } from "@/app/components/card/card.component";
 import { CategoriesDisplayToHome } from "@/app/components/categories-display/categories-display.component";
 import { HeroSection } from "@/app/components/heroSection/hero-section.component";
+import { Partner } from "@/app/components/partner/partner.component";
 import { TabSelectorRelatedProduct } from "@/app/components/tab-selector/tab-selector.component";
 import { Alert } from "@/app/components/ui/alert/alert.ui";
 import { Button } from "@/app/components/ui/button/button.ui";
 import { Carousel } from "@/app/components/ui/carousel/carousel.ui";
 import { Divider } from "@/app/components/ui/divider/divider.ui";
+
 
 import styles from "./page.module.scss";
 
@@ -53,6 +57,60 @@ export default async function Home (): Promise<JSX.Element> {
             )}
         </div>
       </section>
+
+      <Partner />
+
+      <section className={styles.relatedProduct}>
+        <div className={styles.titleContainer}>
+          <Divider variant="relatedProduct"/>
+          <h2>Produtos Relacionados</h2>
+          <Divider variant="relatedProduct"/>
+        </div>
+
+        <Link className={styles.see_all} href="#">Ver Tudo</Link>
+
+        <div className={styles.productsListContainer}>
+          {error ? (
+            <Alert variant="error" title="Ocorreu um Erro">
+              {error}
+            </Alert>) 
+            : (
+              <Carousel>
+                {products.map((product) => (
+                  <CardProduct key={product.productName} productName={product.productName} price={product.price} descriptionShort={product.descriptionShort} photo={product.photo} />
+                ))}
+              </Carousel>
+            )}
+        </div>
+      </section>
+
+      <Partner />
+      <Brands />
+
+      <section className={styles.relatedProduct}>
+        <div className={styles.titleContainer}>
+          <Divider variant="relatedProduct"/>
+          <h2>Produtos Relacionados</h2>
+          <Divider variant="relatedProduct"/>
+        </div>
+
+        <Link className={styles.see_all} href="#">Ver Tudo</Link>
+
+        <div className={styles.productsListContainer}>
+          {error ? (
+            <Alert variant="error" title="Ocorreu um Erro">
+              {error}
+            </Alert>) 
+            : (
+              <Carousel>
+                {products.map((product) => (
+                  <CardProduct key={product.productName} productName={product.productName} price={product.price} descriptionShort={product.descriptionShort} photo={product.photo} />
+                ))}
+              </Carousel>
+            )}
+        </div>
+      </section>
+          
     </main> 
   );
 }
